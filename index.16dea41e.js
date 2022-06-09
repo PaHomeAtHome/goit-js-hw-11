9550e1155c19a463fec5bd3a61975ff3bb3e16ac
PaHomeAtHome <100218289+PaHomeAtHome@users.noreply.github.com>
2022-06-09 16:06:22 +0000

    Deploying to gh-pages from @ PaHomeAtHome/goit-js-hw-11@b5ca171867db85318c4d21b37c1cc71053f65a96 🚀

---
+++ index.16dea41e.js
@@ -0,0 +1,2 @@
+fetch("https://pixabay.com/api/?key=27953461-d4616364e0672ac878ff8b77d&q=cat&image_type=photo&orientation=horizontal&safesearch=true").then((o=>o.json())).then(console.log).catch(console.log);
+//# sourceMappingURL=index.16dea41e.js.map
