702c41c6f6ce3c891b5099c2bc78bd7dfc7b7796
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-09 19:17:02 +0300

    images can be recieved

--- src/index.js
+++ src/index.js
@@ -4,9 +4,21 @@ const imageOrientation  = `horizontal`
 const safeSearch = `true`
 const searchParameter = `cat`
 
-fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${searchParameter}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safeSearch}`).then(response => response.json()).then(console.log).catch(console.log)
+const body = document.querySelector(`body`)
 
-function makeImage(image) {
-body.innerHTML = `<img src=${image[0]} alt="cat">`
+fetch(`https://pixabay.com/api/?key=${API_KEY}&q=${searchParameter}&image_type=${imageType}&orientation=${imageOrientation}&safesearch=${safeSearch}`)
+    .then(response => response.json())
+    .then(makeImage)
+    .catch(console.log)
+
+function makeImage(images) {
+
+    images.hits.map(hit => {
+        const largeImage = hit.largeImageURL
+        body.insertAdjacentHTML("beforeend", `<img src=${largeImage} alt=${searchParameter} width="300">`);
+    })
+    
+console.log(images)
+
 }
 
